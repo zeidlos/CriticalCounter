0638e50afabc3ff8ee2e35ddd600d1450c9ddd4e
Julius Zeidler <julius.zeidler@starfinanz.de>
2017-09-23 04:32:30 +0200

    feat(counter): Resets the counter if new location
    If you moved more than 100m since adding the last bike, the counter will reset.
    
    closes #4

--- src/pages/counter/counter.ts
+++ src/pages/counter/counter.ts
@@ -14,7 +14,7 @@ import { Bike } from './../../models/bike';
 export class CounterPage {
   bike: Bike;
   bikeCount: number = 0;
-  bikes: [Bike];
+  lastGeopoint: any = {lat: 52.520007, long: 13.404954};
 
   location: any;
 
@@ -22,7 +22,7 @@ export class CounterPage {
   bikesRef: FirebaseListObservable<any>;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     private db: AngularFireDatabase,
     private geoLocation: Geolocation,
@@ -31,37 +31,69 @@ export class CounterPage {
     this.bikesRef.subscribe();
     this.watch = this.geoLocation.watchPosition();
     this.watch.subscribe((data) => {
+      // Debugging output, please leave
       console.log(data);
       this.location = data;
     })
   }
 
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CounterPage');
+  // Taken from https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula and adapted to give metres instead of kilometres
+  getDistanceFromLatLonInM(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // this.deg2rad below
+    var dLon = this.deg2rad(lon2-lon1);
+    var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c * 1000; // Distance in m
+    return d;
   }
 
-  getLocation(): any {
-    this.geoLocation.getCurrentPosition()
-      .then(
-        (res) => {
-          console.log('Location: ', res);
-          return res;
-        }
-      ).catch(
-        (err) => {
-          console.log('Error getting location: ', err);
-        }
-      )
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
   }
 
+  resetCounter() {
+    this.bikeCount = 0;
+  }
+
+  checkDistance(currentLat, currentLong) {
+    if(
+      this.getDistanceFromLatLonInM(
+        currentLat,
+        currentLong,
+        this.lastGeopoint.lat,
+        this.lastGeopoint.long,
+      ) > 100
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   // Adds a new bike to the current count
   // Only avaible if device has a location,
   // so no error handling required.
+
+  // Also only resets the counter after having moved more than
+  // 100m and also clicking to re-count
   addBike() {
+    if(this.checkDistance(this.location.coords.latitude, this.location.coords.longitude)) {
+      console.log('Distance is huuuge!');
+      this.resetCounter();
+    }
+
+    this.lastGeopoint = {
+      lat: this.location.coords.latitude,
+      long: this.location.coords.longitude,
+    };
+
     this.bikeCount++;
-    this.bike = { 
+    this.bike = {
       coords: {
         accuracy: this.location.coords.accuracy,
         altitude: this.location.coords.altitude,
@@ -78,11 +110,6 @@ export class CounterPage {
     this.bikesRef.push(this.bike);
   }
 
-  // Starts a new count to the current mass
-  // You start a new count if you are at a new location and start to count bikes again.
-  addCount() {
-
-  }
 
   // Navigates to a page. Who would have guessed? ;)
   navigateTo(page) {
